860f23e1690a5c29fbd6cd20bcb531981829d83e
Bruce Brown <brucebrown715@gmail.com>
2020-08-26 13:06:50 -0500

    commit

---
+++ 70_Combine/Main.java
@@ -0,0 +1,30 @@
+package pkg70_combinearraylist;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+
+        list1.add(4);
+        list1.add(3);
+
+        list2.add(5);
+        list2.add(10);
+        list2.add(7);
+
+        combine(list1, list2);
+
+        System.out.println(list1); // prints [4, 3, 5, 10, 7]
+
+        System.out.println(list2); // prints [5, 10, 7]
+    }
+    
+    
+    public static void combine(ArrayList<Integer> first, ArrayList<Integer> second){
+        second.addAll(first);
+    }
+}
