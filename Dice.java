5c63be34d40918e46d73d1ff99324f5b87b5f116
Bruce Brown <brucebrown715@gmail.com>
2020-11-19 08:46:31 -0800

    commit

---
+++ Dice.java
@@ -0,0 +1,19 @@
+package pkg80_dice;
+import java.util.Random;
+
+
+public class Dice {
+    
+    private Random random;
+    private int numberOfSides;
+    
+    public Dice(int numberOfSides){
+        this.numberOfSides = numberOfSides;
+        random = new Random();
+
+    }
+    
+    public int roll(){
+        return this.random.nextInt(this.numberOfSides)+1;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,19 @@
+package pkg80_dice;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        
+        Dice dice = new Dice(6);
+
+        int i = 0;
+        while ( i < 10 ) {
+            System.out.println( dice.roll() );
+            i++;
+        }
+        
+    }
+    
+}
